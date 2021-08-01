d23a66e56f97c2334ae54b234215f7a578ab44cc
0187773933 <collincerbus@student.olympic.edu>
2021-08-01 03:04:07 -0400

    34

--- dockerHubPublish.sh
+++ dockerHubPublish.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #sudo docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7,linux/arm/v6 -t raspi-motion-tracker-frame-consumer:latest --push .
-sudo docker buildx build --platform linux/arm/v6 -t xp6qhg9fmuolztbd2ixwdbtd1/raspi-motion-tracker-frame-consumer:arm32 --push .
+sudo docker buildx build -m 8g --platform linux/arm/v6 -t xp6qhg9fmuolztbd2ixwdbtd1/raspi-motion-tracker-frame-consumer:arm32 --push .
--- install_venv.sh
+++ install_venv.sh
@@ -6,14 +6,14 @@
 # source ./venv/bin/activate
 
 /home/morphs/.pyenv/versions/3.7.3/bin/python3.7 -m pip install ./wheels/numpy-1.19.5-cp37-cp37m-linux_armv7l.whl
-/home/morphs/.pyenv/versions/3.7.3/bin/python3.7 -m pip install install ./wheels/h5py-2.10.0-cp37-cp37m-linux_armv7l.whl
-/home/morphs/.pyenv/versions/3.7.3/bin/python3.7 -m pip install install ./wheels/grpcio-1.32.0-cp37-cp37m-linux_armv7l.whl
+/home/morphs/.pyenv/versions/3.7.3/bin/python3.7 -m pip install ./wheels/h5py-2.10.0-cp37-cp37m-linux_armv7l.whl
+/home/morphs/.pyenv/versions/3.7.3/bin/python3.7 -m pip install ./wheels/grpcio-1.32.0-cp37-cp37m-linux_armv7l.whl
 # cat ./wheels/tensorflow-2.4.0-cp37-cp37m-linux_armv7l.whl.zip* > ./wheels/tensorflow.zip
 # unzip ./wheels/tensorflow.zip
 # mv ./tensorflow-2.4.0-cp37-cp37m-linux_armv7l.whl ./wheels/
 # rm ./wheels/tensorflow.zip
 wget https://39363.org/WHEELS/RASPI/tensorflow-2.4.0-cp37-cp37m-linux_armv7l.whl -O ./wheels/tensorflow-2.4.0-cp37-cp37m-linux_armv7l.whl
-/home/morphs/.pyenv/versions/3.7.3/bin/pipython3.7 -m pip install install ./wheels/tensorflow-2.4.0-cp37-cp37m-linux_armv7l.whl
+/home/morphs/.pyenv/versions/3.7.3/bin/pipython3.7 -m pip install ./wheels/tensorflow-2.4.0-cp37-cp37m-linux_armv7l.whl
 /home/morphs/.pyenv/versions/3.7.3/bin/python3.7 -m pip install install ./wheels/sanic-21.6.0-py3-none-any.whl
 /home/morphs/.pyenv/versions/3.7.3/bin/python3.7 -m pip install ./wheels/uvloop-0.15.2-cp37-cp37m-linux_armv7l.whl
 /home/morphs/.pyenv/versions/3.7.3/bin/python3.7 -m pip install ./wheels/httptools-0.2.0-cp37-cp37m-linux_armv7l.whl
