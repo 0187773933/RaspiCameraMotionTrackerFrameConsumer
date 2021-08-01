b5d230a8263af6a7815d668b50cd6b4c3f528373
0187773933 <collincerbus@student.olympic.edu>
2021-08-01 03:05:11 -0400

    35

--- install_venv.sh
+++ install_venv.sh
@@ -13,7 +13,7 @@
 # mv ./tensorflow-2.4.0-cp37-cp37m-linux_armv7l.whl ./wheels/
 # rm ./wheels/tensorflow.zip
 wget https://39363.org/WHEELS/RASPI/tensorflow-2.4.0-cp37-cp37m-linux_armv7l.whl -O ./wheels/tensorflow-2.4.0-cp37-cp37m-linux_armv7l.whl
-/home/morphs/.pyenv/versions/3.7.3/bin/pipython3.7 -m pip install ./wheels/tensorflow-2.4.0-cp37-cp37m-linux_armv7l.whl
+/home/morphs/.pyenv/versions/3.7.3/bin/python3.7 -m pip install ./wheels/tensorflow-2.4.0-cp37-cp37m-linux_armv7l.whl
 /home/morphs/.pyenv/versions/3.7.3/bin/python3.7 -m pip install install ./wheels/sanic-21.6.0-py3-none-any.whl
 /home/morphs/.pyenv/versions/3.7.3/bin/python3.7 -m pip install ./wheels/uvloop-0.15.2-cp37-cp37m-linux_armv7l.whl
 /home/morphs/.pyenv/versions/3.7.3/bin/python3.7 -m pip install ./wheels/httptools-0.2.0-cp37-cp37m-linux_armv7l.whl
